dba376271225329929c77fc97e650b536bea99a9
evanfcrane <evan.crane@izenda.com>
2020-08-10 19:12:09 -0400

    Initial Upgrade of all main files

--- AngularStarterKitWeb/src/app/export/export.component.ts
+++ AngularStarterKitWeb/src/app/export/export.component.ts
@@ -1,5 +1,9 @@
 import { Component, AfterViewInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
+
+import '../../assets/izenda/izenda_common';
+import '../../assets/izenda/izenda_locales';
+import '../../assets/izenda/izenda_vendors';
 declare var require: any;
 const IzendaSynergy = require('../../assets/izenda/izenda_ui');
 
@@ -9,30 +13,28 @@ const IzendaSynergy = require('../../assets/izenda/izenda_ui');
   styleUrls: ['./export.component.css']
 })
 export class ExportComponent implements AfterViewInit {
-
   currentUserContext: any = {};
-  reportPartId: string;
-   constructor( private router: Router, private activatedRoute: ActivatedRoute) {
-   }
+    reportPartId: string;
+    constructor(private router: Router, private activatedRoute: ActivatedRoute) {
+    }
 
-   ngAfterViewInit() {
-       console.log(this.activatedRoute);
-       this.activatedRoute.params.subscribe((params: Params) => {
-           this.reportPartId = params.id;
-       });
+    ngAfterViewInit() {
+        console.log(this.activatedRoute);
+        this.activatedRoute.params.subscribe((params: Params) => {
+            this.reportPartId = params['id'];
+        });
 
-       this.activatedRoute.queryParams.subscribe((params: Params) => {
-           const token = params.token;
-           this.currentUserContext = { token};
-       });
+        this.activatedRoute.queryParams.subscribe((params: Params) => {
+            let token = params['token'];
+            this.currentUserContext = { token: token };
+        });
 
-       console.log(this.reportPartId);
-       console.log(this.currentUserContext);
-       IzendaSynergy.setCurrentUserContext(this.currentUserContext);
-       IzendaSynergy.renderReportPart(document.getElementById('izenda-export-reportpart'), {
-               id: this.reportPartId,
-               useQueryParam: true,
-           });
-   }
-
+        console.log(this.reportPartId);
+        console.log(this.currentUserContext);
+        IzendaSynergy.setCurrentUserContext(this.currentUserContext);
+        IzendaSynergy.renderReportPart(document.getElementById('izenda-export-reportpart'), {
+            "id": this.reportPartId,
+            "useQueryParam": true,
+        });
+    }
 }
